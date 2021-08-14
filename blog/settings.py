d7262fbd6e693893e9ed0e4baff46393864f16ac
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-08-15 02:18:47 +0530

    update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_FILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
     os.path.join(BASE_DIR, 'media')
 ]
